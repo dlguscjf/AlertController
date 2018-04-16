6ce95e6ee37b6a286be58d4e9099ec43f2b78c02
D7703_06 <d7703_06@D7703-06ui-iMac.local>
2018-04-16 15:36:59 +0900

    Add alert

--- AlertController/ViewController.swift
+++ AlertController/ViewController.swift
@@ -20,6 +20,16 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func alert(_ sender: Any) {
+        let alert = UIAlertController(title:"선택", message: "확인", preferredStyle: .alert)
+        //alert 생성 제목,내용,중앙출력 or 하단출력
+        let okAction = UIAlertAction(title: "확인", style: .default, handler: {(action:  UIAlertAction!) in self.view.backgroundColor = UIColor.red})
+        //alert 액션 생성 제목,스타일 : 종료, 선택 후 액션
+       alert.addAction(okAction)
+        //alert에 okAction생성
+        self.present(alert, animated: true, completion: nil)
+        //alert 출력
+    }
+    
 }
 
